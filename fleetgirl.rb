1fcbd502ad3d4135d4cfaf6467e562e4d096e617
xdotzv <xdotzv@qq.com>
2015-04-30 01:11:21 +0800

    tab to space

--- fleetgirl.rb
+++ fleetgirl.rb
@@ -4,99 +4,99 @@ require './basefleetgirl'
 
 class FleetGirl < BaseFleetGirl 
 
-	def get_fleet_info(fleet_id)
-		data = get_init_data
-		ships = data["fleetVo"][fleet_id-1]["ships"]
-		ships.map { |id| data["userShipVO"].find { |x| x["id"] == id} }
-	end
+    def get_fleet_info(fleet_id)
+        data = get_init_data
+        ships = data["fleetVo"][fleet_id-1]["ships"]
+        ships.map { |id| data["userShipVO"].find { |x| x["id"] == id} }
+    end
 
-	def combat_by_path(fleet_id, mission_id, path, formations)
-		base_node_id = "#{mission_id}02".to_i
-		supply_fleet(fleet_id)
-		fleet_info = get_fleet_info(fleet_id)
+    def combat_by_path(fleet_id, mission_id, path, formations)
+        base_node_id = "#{mission_id}02".to_i
+        supply_fleet(fleet_id)
+        fleet_info = get_fleet_info(fleet_id)
 
-		max_hps = fleet_info.map { |x| x["battlePropsMax"]["hp"]}
-		hps = fleet_info.map { |x| x["battleProps"]["hp"]}
+        max_hps = fleet_info.map { |x| x["battlePropsMax"]["hp"]}
+        hps = fleet_info.map { |x| x["battleProps"]["hp"]}
 
-		return if pve_start(fleet_id, mission_id) == false
-		0.upto(path.length-1) do |i|
-			0.upto(fleet_info.length-1) do |k|
-				return if hps[k] < 0.5 * max_hps[k]
-			end
-			node_id = get_next_node
-			#binding.pry
-			return true if node_id - base_node_id != path[i].ord - "A".ord
-			#only go for night war at the last battle
-			night_war = path.length-i==1 ? 1 : 0
-			r = pve_battle fleet_id, node_id, formations[i], night_war
-			hps = r["warResult"]["selfShipResults"].map { |x| x["hp"] } if r.nil? == false
-			p hps
-			p max_hps
-			0.upto(fleet_info.length-1) do |k|
-				return false if hps[k] < 0.5 * max_hps[k]
-			end
-			sleep 12
-		end
-		pve_end
-		true
-	end
-	def combat(fleet_id=1, mission_id=204, supply=true, night_war=0)
-		supply_fleet(fleet_id) if supply
+        return if pve_start(fleet_id, mission_id) == false
+        0.upto(path.length-1) do |i|
+            0.upto(fleet_info.length-1) do |k|
+                return if hps[k] < 0.5 * max_hps[k]
+            end
+            node_id = get_next_node
+            #binding.pry
+            return true if node_id - base_node_id != path[i].ord - "A".ord
+            #only go for night war at the last battle
+            night_war = path.length-i==1 ? 1 : 0
+            r = pve_battle fleet_id, node_id, formations[i], night_war
+            hps = r["warResult"]["selfShipResults"].map { |x| x["hp"] } if r.nil? == false
+            p hps
+            p max_hps
+            0.upto(fleet_info.length-1) do |k|
+                return false if hps[k] < 0.5 * max_hps[k]
+            end
+            sleep 12
+        end
+        pve_end
+        true
+    end
+    def combat(fleet_id=1, mission_id=204, supply=true, night_war=0)
+        supply_fleet(fleet_id) if supply
 
-		if pve_start(fleet_id, mission_id)
-			node_id = get_next_node
-			r = get "pve/deal/#{node_id}/#{fleet_id}/2"
-			night_war = r["warReport"]["canDoNightWar"] if night_war==1
-			get "pve/getWarResult/#{night_war}"
-			#pry.binding
-			#puts JSON.parse(r)
-			get "pve/pveEnd/"
-		end
-	end
+        if pve_start(fleet_id, mission_id)
+            node_id = get_next_node
+            r = get "pve/deal/#{node_id}/#{fleet_id}/2"
+            night_war = r["warReport"]["canDoNightWar"] if night_war==1
+            get "pve/getWarResult/#{night_war}"
+            #pry.binding
+            #puts JSON.parse(r)
+            get "pve/pveEnd/"
+        end
+    end
 
 
-	def get_damaged_ships(r=nil)
-		r = get "initData" if r.nil?
-		r["userShipVO"].select { |x| x["battleProps"]["hp"] < x["battlePropsMax"]["hp"] and x["status"] != 2}.map{ |x| x["id"]}
-	end
+    def get_damaged_ships(r=nil)
+        r = get "initData" if r.nil?
+        r["userShipVO"].select { |x| x["battleProps"]["hp"] < x["battlePropsMax"]["hp"] and x["status"] != 2}.map{ |x| x["id"]}
+    end
 
-	def repair()
-		#sleep 30
-		data = get "api/initData"
+    def repair()
+        #sleep 30
+        data = get "api/initData"
 
-		damaged = get_damaged_ships data
+        damaged = get_damaged_ships data
 
-		repair_dock = data["repairDockVo"].select { |x| x["locked"] == 0 and x["shipId"].nil? }
+        repair_dock = data["repairDockVo"].select { |x| x["locked"] == 0 and x["shipId"].nil? }
 
-		while repair_dock.length > 0 and damaged.length > 0
-			dock = repair_dock.shift
-			ship = damaged.shift
+        while repair_dock.length > 0 and damaged.length > 0
+            dock = repair_dock.shift
+            ship = damaged.shift
 
-			get "boat/repair/#{ship}/#{dock["id"]}"
+            get "boat/repair/#{ship}/#{dock["id"]}"
 
-			data = JSON.parse(get "api/initData")
-			damaged = data["userShipVO"].select { |x| x["battleProps"]["hp"] < x["battlePropsMax"]["hp"] and x["status"] == 0}.map{ |x| x["id"]}
-			repair_dock = data["repairDockVo"].select { |x| x["locked"] == 0 and x["endTime"] < Time.now.to_i }
-		end
-	end
+            data = JSON.parse(get "api/initData")
+            damaged = data["userShipVO"].select { |x| x["battleProps"]["hp"] < x["battlePropsMax"]["hp"] and x["status"] == 0}.map{ |x| x["id"]}
+            repair_dock = data["repairDockVo"].select { |x| x["locked"] == 0 and x["endTime"] < Time.now.to_i }
+        end
+    end
 
-	def fleet_status(fleet_number)
-		r = get "api/initData"
-		ships = r["fleetVo"][fleet_number-1]["ships"]
-		r["userShipVO"].select{ |x| ships.include? x["id"]}
-	end
+    def fleet_status(fleet_number)
+        r = get "api/initData"
+        ships = r["fleetVo"][fleet_number-1]["ships"]
+        r["userShipVO"].select{ |x| ships.include? x["id"]}
+    end
 
-	def heavy_damaged?(fleet_number=1)
-		status = fleet_status(fleet_number)
-		p status.map { |x| (x["battleProps"]["hp"].to_f / x["battlePropsMax"]["hp"]).round(2) }
-		status.any? { |x| x["battleProps"]["hp"] < 0.5*x["battlePropsMax"]["hp"]}
-	end
+    def heavy_damaged?(fleet_number=1)
+        status = fleet_status(fleet_number)
+        p status.map { |x| (x["battleProps"]["hp"].to_f / x["battlePropsMax"]["hp"]).round(2) }
+        status.any? { |x| x["battleProps"]["hp"] < 0.5*x["battlePropsMax"]["hp"]}
+    end
 
-	def pvp_all()
-		get_pvp_list.each do |opponent|
-			pvp_battle opponent
-		end
-	end
+    def pvp_all()
+        get_pvp_list.each do |opponent|
+            pvp_battle opponent
+        end
+    end
 # def pvp():
 #     print json_with_cookie("pvp/getChallengeList/")
 #     fleetIds = map(lambda x: x["uid"], json_with_cookie("pvp/getChallengeList/")["list"])
@@ -104,41 +104,41 @@ class FleetGirl < BaseFleetGirl
 #         print json_with_cookie("pvp/challenge/%s/%d/%d" % (fleetId, 1, 1)) # fleetId, myfleetID, formation
 #         print json_with_cookie("pvp/getWarResult/0")
 
-	def find_pupils()
-		get("api/initData")["userShipVO"].select { |x| x["level"] == 1 and x["fleetId"] == 0 and x["exp"] == 0 and x["isLocked"] == 0}
-	end
+    def find_pupils()
+        get("api/initData")["userShipVO"].select { |x| x["level"] == 1 and x["fleetId"] == 0 and x["exp"] == 0 and x["isLocked"] == 0}
+    end
 
-	def dark()
-		fleet_id = 1
+    def dark()
+        fleet_id = 1
 
-		pupils = find_pupils
+        pupils = find_pupils
 
-		5.downto(1) do |index|
+        5.downto(1) do |index|
       remove_ship fleet_id, index
-		end
+        end
 
-		if pupils.length > 6
-			tmps = pupils[6..-1].map { |x| x['id'] }
+        if pupils.length > 6
+            tmps = pupils[6..-1].map { |x| x['id'] }
       dismantle_ships tmps, 1
-		end
+        end
 
-		pupils = pupils[0..5]
+        pupils = pupils[0..5]
 #may replace itself
-		pupils[0..-2].each do |x|
+        pupils[0..-2].each do |x|
       change_ship fleet_id, x["id"], 0
-			5.times { combat 1, 101, false, 1 }
-		end
+            5.times { combat 1, 101, false, 1 }
+        end
 
     change_ship fleet_id, pupils[-1]['id'], 0
-		tmps = pupils[0..-2].map { |x| x['id'] }
+        tmps = pupils[0..-2].map { |x| x['id'] }
     dismantle_ships tmps, 1
-	end
+    end
 
-	def good_night()
-		1.upto(4) do |i|
-			supply_fleet i
-		end
-		r = get_init_data
+    def good_night()
+        1.upto(4) do |i|
+            supply_fleet i
+        end
+        r = get_init_data
     repair_dock = r["repairDockVo"]
 
     ships = get_damaged_ships(r)
